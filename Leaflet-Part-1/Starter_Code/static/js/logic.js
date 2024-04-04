08cc3c10c8894a494ea8970734e1973df415e808
clorden1 <cpordenla@gmail.com>
2024-04-04 12:53:54 -0500

    added legend to map

--- Leaflet-Part-1/Starter_Code/static/js/logic.js
+++ Leaflet-Part-1/Starter_Code/static/js/logic.js
@@ -17,6 +17,7 @@ function markerSize(magnitude) {
     return Math.sqrt(magnitude) * 9;
 };
 
+//Customize marker color based on depth
 function markerColor(depth) {
     if (depth < 11) {
         color = "Green";
@@ -34,8 +35,9 @@ function markerColor(depth) {
     return color
 };
 
+//Create Markers for map
 d3.json(url).then(function (data){
-    L.geoJson(data, {
+    geojson = L.geoJson(data, {
         style: function(feature) {
             return {
               radius: markerSize(feature.properties.mag),
@@ -51,4 +53,28 @@ d3.json(url).then(function (data){
             <h3>Location: ${feature.geometry.coordinates.slice(0,2).toLocaleString()}`).addTo(myMap);;
         }
     })
+// Define legend content
+var legendContent = '<strong>Earthquake Depth Legend</strong><br>' +
+    '<i style="background: Green"></i> Depth &lt; 11 km<br>' +
+    '<i style="background: YellowGreen"></i> 11 km &le; Depth &lt; 31 km<br>' +
+    '<i style="background: Yellow"></i> 31 km &le; Depth &lt; 51 km<br>' +
+    '<i style="background: Orange"></i> 51 km &le; Depth &lt; 71 km<br>' +
+    '<i style="background: OrangeRed"></i> 71 km &le; Depth &lt; 91 km<br>' +
+    '<i style="background: Red"></i> Depth &ge; 91 km';
+
+// Create legend
+var legend = L.control({position: 'bottomright'});
+
+legend.onAdd = function (map) {
+  var div = L.DomUtil.create('div', 'info legend');
+  div.innerHTML = legendContent;
+  return div;
+};
+
+legend.addTo(myMap);
+
+//Style Legend
+var legendStyle = document.createElement('style');
+legendStyle.innerHTML = '.legend i { display: inline-block; width: 20px; height: 20px; margin-right: 8px; }';
+document.getElementsByTagName('head')[0].appendChild(legendStyle);
 });
